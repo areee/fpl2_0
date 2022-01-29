46e9edc387770f992dbc3cdc1c6a215d34750b13
areee <10089872+areee@users.noreply.github.com>
2022-01-29 10:25:45 +0200

    Refactor (move CustomBottomAppBar to an own file)

---
+++ lib/customBottomAppBar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomAppBar extends StatelessWidget {
+  const CustomBottomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      shape: const CircularNotchedRectangle(),
+      color: Colors.blue,
+      child: IconTheme(
+        data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+        child: Row(
+          children: <Widget>[
+            const Spacer(
+              flex: 3,
+            ),
+            IconButton(
+              tooltip: 'Randomize',
+              icon: const Icon(Icons.shuffle),
+              onPressed: () {},
+            ),
+            const Spacer(
+              flex: 1,
+            ),
+            IconButton(
+              tooltip: 'Stop',
+              icon: const Icon(Icons.stop),
+              onPressed: () {},
+            ),
+            const Spacer(
+              flex: 3,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'customBottomAppBar.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -45,44 +47,6 @@ class HomePage extends StatelessWidget {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: const CustomBottomAppBar(),
-    );
-  }
-}
-
-class CustomBottomAppBar extends StatelessWidget {
-  const CustomBottomAppBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomAppBar(
-      shape: const CircularNotchedRectangle(),
-      color: Colors.blue,
-      child: IconTheme(
-        data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
-        child: Row(
-          children: <Widget>[
-            const Spacer(
-              flex: 3,
-            ),
-            IconButton(
-              tooltip: 'Randomize',
-              icon: const Icon(Icons.shuffle),
-              onPressed: () {},
-            ),
-            const Spacer(
-              flex: 1,
-            ),
-            IconButton(
-              tooltip: 'Stop',
-              icon: const Icon(Icons.stop),
-              onPressed: () {},
-            ),
-            const Spacer(
-              flex: 3,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
