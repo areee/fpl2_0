0252f87156f95831b248b9df8186c60bc44c9f8d
areee <10089872+areee@users.noreply.github.com>
2022-02-06 21:20:22 +0200

    Update settings_page (add initial counter duration textField)

--- lib/view/settings_page.dart
+++ lib/view/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -9,19 +10,24 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: Center(
+      body: Container(
+        padding: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Settings',
-              style: TextStyle(fontSize: 24),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
+            TextField(
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+              ],
+              decoration: const InputDecoration(
+                labelText: 'Counter duration',
+                border: OutlineInputBorder(),
+                hintText: 'Enter time in seconds',
+              ),
+              onChanged: (text) {
+                print('Output of the number field: $text');
               },
-              child: const Text('Go back'),
             ),
           ],
         ),
