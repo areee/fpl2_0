867b042a20b6f42c307ee42e2bbdab5e0aafc7f1
areee <10089872+areee@users.noreply.github.com>
2022-05-25 09:47:41 +0300

    Add missing Spring & Summer themes & use them

--- lib/components/custom_app_bar.dart
+++ lib/components/custom_app_bar.dart
@@ -3,16 +3,9 @@ import 'package:flutter/material.dart';
 PreferredSizeWidget customAppBar(
     BuildContext context, Widget title, List<Widget>? actions) {
   return AppBar(
+    backgroundColor: Theme.of(context).colorScheme.primary,
     title: title,
     centerTitle: true,
-    flexibleSpace: Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          repeat: ImageRepeat.repeatX,
-          image: AssetImage('assets/images/lumihiutale.png'),
-        ),
-      ),
-    ),
     actions: actions,
   );
 }
