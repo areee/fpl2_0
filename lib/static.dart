6bbaaf6bfd3f0cd576e3cf4e33c5f04b1d06a417
areee <10089872+areee@users.noreply.github.com>
2022-05-29 21:09:24 +0300

    Add test data for multi avatar participants

--- lib/static.dart
+++ lib/static.dart
@@ -8,3 +8,33 @@ enum TimerStatus {
 
 /// The 1st June 2022
 DateTime firstSummer2022Day() => DateTime(2022, 6, 1);
+
+/// Test data for multi avatar participants
+List<String> testParticipantNamesInList = [
+  'Amy',
+  'Bob',
+  'Cathy',
+  'David',
+  'Eve',
+  'Frank',
+  'George',
+  'Hannah',
+  'Irene',
+  'Jack',
+  'Kathy',
+  'Linda',
+  'Mandy',
+  'Nancy',
+  'Olivia',
+  'Penny',
+  'Quinn',
+  'Ruth',
+  'Sally',
+  'Tina',
+  'Ursula',
+  'Vicky',
+  'Wendy',
+  'Xavier',
+  'Yvonne',
+  'Zoe'
+];
