5cadc5b8bc0846b9c3055748edf92184f022eb13
areee <10089872+areee@users.noreply.github.com>
2022-12-21 08:10:41 +0200

    Update Christmas 2022 theme & clean

--- lib/static.dart
+++ lib/static.dart
@@ -6,9 +6,6 @@ enum TimerStatus {
   stopped,
 }
 
-/// The 1st June 2022
-DateTime firstSummer2022Day() => DateTime(2022, 6, 1);
-
 /// Test data for multi avatar participants
 List<String> testParticipantNamesInList = [
   'Amy',
