585b3130e05d562c16c145e901b1a335921b58ae
Arttu Ylhävuori <10089872+areee@users.noreply.github.com>
2023-01-15 22:49:46 +0200

    Some small fixes (#14)
    * Remove an asset from pubspec.yaml
    
    * Add dependabot.yml
    
    * Add 'getAppBarText' & use it

--- lib/static.dart
+++ lib/static.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 enum AppBarValues { settings, about }
 
 enum TimerStatus {
@@ -35,3 +37,54 @@ List<String> testParticipantNamesInList = [
   // 'Yvonne',
   // 'Zoe'
 ];
+
+/// Returns days of the week in English
+List<String> weekDays = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
+
+/// Returns the day of the week in Finnish
+String getWeekDay(int day) {
+  return weekDays[day];
+}
+
+/// Returns adjectives in English
+List<String> adjectives = [
+  'amazing',
+  'beautiful',
+  'crazy',
+  'delightful',
+  'elegant',
+  'fabulous',
+  'gorgeous',
+  'happy',
+  'intelligent',
+  'joyful',
+  'kind',
+  'lovely',
+  'magnificent',
+  'nice',
+  'optimistic',
+  'perfect',
+  'quiet',
+  'rational',
+  'silly',
+  'talented',
+  'unique',
+  'victorious',
+  'wonderful',
+  'xenial',
+  'young',
+  'zealous',
+];
+
+/// Returns a random adjective
+String getAdjective() {
+  return adjectives[Random().nextInt(adjectives.length)];
+}
